73b6b69a49d8b88f4cf12fd79d491f38036443c4
127.0.0.1 <info@gcoin.io>
2014-11-15 17:03:52 -0600

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define GCOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "GCOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
